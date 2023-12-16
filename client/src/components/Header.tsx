e679dcefc63becd32b2a60585a0ada49e01b9d73
Richard Rino <richardvrino@gmail.com>
2023-12-16 13:15:24 -0700

    Add Header component with basic styling

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,17 @@
+import '../index.scss';
+
+const Header = () => {
+	return (
+		<header className='header'>
+			<h1>LiftLogix</h1>
+			<nav>
+				<ul className='nav-list'>
+					<li>Home</li>
+					<li>Log</li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
